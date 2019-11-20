4833e4620ec800573c32af0e57306198ddddf801
slizbeth <stefany.lizbeth@gmail.com>
2019-11-20 16:45:06 +0100

    add new functions js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 'use strict';
+
+//declaraion de variables
 const numberInputElement = document.querySelector('#numberInput');
 const buttonElement = document.querySelector('#button');
+const firsTextElement  = document.querySelector('#firsText');
 const secondTextElement = document.querySelector('#secondText');
+const countElement  = document.querySelector('#counter');
 let cont = 0;
 
 //funcion que genera num aleatorio entre 1 y 100
@@ -13,33 +17,50 @@ const control = function (){
   let numberInputValue = numberInputElement.value;
   let numberParseInt = parseInt(numberInputValue);
   //console.log(typeof(numberParseInt));
-  console.log(randomNumber); //3.mostrar número aleatorio
-  console.log(numberParseInt); //4.contenido input
+  console.log("Número aleatorio: " + randomNumber); //3.mostrar número aleatorio
+  console.log("Número introducido: " + numberParseInt); //4.contenido input
 
   if(numberParseInt >= randomNumber){
     secondTextElement.innerHTML= 'Demasiado alto.';
-    console.log('Demasiado alto.');
+    //console.log('Demasiado alto.');
   }else if(numberParseInt <= randomNumber){
     secondTextElement.innerHTML= 'Demasiado bajo.';
-    console.log('Demasiado bajo.');
+    //console.log('Demasiado bajo.');
   }else if(numberParseInt === randomNumber){
     secondTextElement.innerHTML= 'Has ganado campeona!!!';
-    console.log('Has ganado campeona!!!');
+    //console.log('Has ganado campeona!!!');
   }else if(numberParseInt >=1 && randomNumber<= 100){
     secondTextElement.innerHTML= 'El número debe estar entre 1 y 100';
-    console.log('El número debe estar entre 1 y 100');
+    //console.log('El número debe estar entre 1 y 100');
   }else{
     secondTextElement.innerHTML= 'No';
-    console.log('No');
+    //console.log('No');
   }
 
-  contClik();
+  //console.log(event.target);
+  let buttonElementClik = event.target;
+  //llamar a la funcion contar clicks
+  contClik(buttonElementClik);
 }
 
-const contClik  = function (){
-  cont+= cont;
-  console.log(cont);
+//funcion que cuentas los cliks del botón
+const contClik  = function (a){
+  cont++;
+  countElement.innerHTML = cont;
+  console.log('Número de veces que ha pulsado el botón' + cont);
+  
 }
 
+//funcion que muestra texto al cargar la página
+const write  = function (){
+  // const chilParagraph = document.createElement('p');  
+  // chilParagraph.innerHTML= 'Pista: Escribe el número y dale a Prueba';
+  // pageElement.appendChild(chilParagraph);
+  firsTextElement.innerHTML = 'Pista: Escribe el número y dale a Prueba';
+}
+
+//listeners?
+window.addEventListener('load', write);
 buttonElement.addEventListener('click', control);
+
 
